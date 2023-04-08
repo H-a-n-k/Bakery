510cb9f1be17070fb06c262195739d7a0d8b6441
H-a-n-k <haunguyen231122@gmail.com>
2023-04-09 00:01:11 +0700

    Profile khach hang + danh gia sp + cai gi do nua

--- Controllers/KhachHangsController.cs
+++ Controllers/KhachHangsController.cs
@@ -4,6 +4,7 @@ using System.Data;
 using System.Data.Entity;
 using System.Data.Entity.Core.Objects;
 using System.Diagnostics.Eventing.Reader;
+using System.EnterpriseServices;
 using System.Linq;
 using System.Net;
 using System.Web;
@@ -19,110 +20,65 @@ using Bakery.Models.ViewModels;
 
 namespace Bakery.Controllers
 {
+    [Authorize]
     public class KhachHangsController : Controller
     {
         private BakeryStoreDBEntities db = new BakeryStoreDBEntities();
 
-        // GET: KhachHangs
-        public ActionResult Index(bool? isActive, string keyword, int? page, int? pageLength)
-        {
-            ObjectParameter count = new ObjectParameter("pageCount", typeof(Int32));
-            var list = db.sp_dskhachhang(count, isActive, keyword, page, pageLength).ToList();
-			ViewBag.TotalPage = Convert.ToInt32(count.Value);
-            return View(list);
-        }
-
         // GET: KhachHangs/Details/5
-        public ActionResult Details(int? id)
+        public ActionResult Details()
         {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
+            int? id = Session["CustomerID"] == null ? null : (int?)Session["CustomerID"];
+            if (id == null) return RedirectToAction("SignIn", "Auth");
 
-            var khachHang = db.sp_profilekhachhang(id).SingleOrDefault();
+			var khachHang = db.sp_profilekhachhang(id).SingleOrDefault();
             if (khachHang == null)
             {
                 return HttpNotFound();
             }
-            return View(khachHang);
+            var ctkh = new CTKHVM();
+            ctkh.kh = khachHang;
+
+            var hds = new List<CTHDVM>();
+            var dshd = db.sp_dshoadon(id, null).ToList();
+            foreach (var x in dshd) {
+                var cthds = db.sp_ds_cthd(x.MaHD).ToList();
+                hds.Add(new CTHDVM() { hd = x, cthds = cthds });
+            }
+            ctkh.hds = hds;
+
+            return View(ctkh);
         }
 
-        // GET: KhachHangs/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
+		// GET: KhachHangs/Create
+        [AllowAnonymous]
+		public ActionResult Create()
+		{
+			return View();
+		}
 
-        // POST: KhachHangs/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(SignUpVM kh)
-        {
-            if (ModelState.IsValid)
-            {
-                db.sp_dkytaikhoan(kh.TenKH, kh.GioiTinh, kh.SoDienThoai, kh.NgaySinh, kh.TaiKhoan, kh.MatKhau);
-                return RedirectToAction("Index");
-            }
+		// POST: KhachHangs/Create
+		// To protect from overposting attacks, enable the specific properties you want to bind to, for 
+		// more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		[AllowAnonymous]
+		public ActionResult Create(SignUpVM kh)
+		{
+			if (ModelState.IsValid)
+			{
+				db.sp_dkytaikhoan(kh.TenKH, kh.GioiTinh, kh.SoDienThoai, kh.NgaySinh, kh.TaiKhoan, kh.MatKhau);
+				return RedirectToAction("Index", "Home");
+			}
 
-            return View(kh);
-        }
+			return View(kh);
+		}
 
-        // GET: KhachHangs/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            var khachHang = db.sp_profilekhachhang(id).SingleOrDefault();
-            if (khachHang == null)
-            {
-                return HttpNotFound();
-            }
-            return View(khachHang);
-        }
-
-        // POST: KhachHangs/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "MaKH,TenKH,GioiTinh,SoDienThoai,NgaySinh")] KhachHang kh)
-        {
-            if (ModelState.IsValid)
-            {
-                db.sp_updatekhachhang(kh.MaKH, kh.TenKH, kh.GioiTinh, kh.SoDienThoai, kh.NgaySinh);
-                return RedirectToAction("Index");
-            }
-            var khachHang = db.sp_profilekhachhang(kh.MaKH).SingleOrDefault();
-			return View(khachHang);
-        }
-
-        // GET: KhachHangs/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            var khachHang = db.sp_profilekhachhang(id).SingleOrDefault();
-            if (khachHang == null)
-            {
-                return HttpNotFound();
-            }
-            return View(khachHang);
-        }
-
-		// POST: KhachHangs/Delete/5
-		[HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            db.sp_deletekhachhang(id);
-            return RedirectToAction("Index");
+        public ActionResult AddReview(List<sp_ds_cthd_Result> reviews) {
+            foreach (var x in reviews) {
+                db.sp_danhGiaSP(x.MaHD, x.MaSP, x.SoSaoDanhGia, x.NoiDungDanhGia);
+			}
+            return RedirectToAction("Details");
         }
 
 		protected override void Dispose(bool disposing)
